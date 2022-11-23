99d697035f63b8899867cd6e08b4bdf5c4cf2f2a
yutotakakura <yutotakakura0320@gmail.com>
2022-11-23 16:31:41 +0900

    webpack-dev-serverを導入

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    }
+  }
 };
